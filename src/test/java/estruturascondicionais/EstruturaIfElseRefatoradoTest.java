f07f669495b195baec6dedddda785a752b1b01d1
monicat <mda263@bol.com.br>
2024-09-10 15:16:29 -0300

    teste unitários: EstruturaElseRefatorada

---
+++ src/test/java/estruturascondicionais/EstruturaIfElseRefatoradoTest.java
@@ -0,0 +1,25 @@
+package estruturascondicionais;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstruturaIfElseRefatoradoTest {
+    @Test
+    void testHoraEntre1e11() {
+        assertEquals("Good morning !!", EstruturaIfElseRefatorado.getMensagemPorHora(5));
+        assertEquals("Good morning !!", EstruturaIfElseRefatorado.getMensagemPorHora(11));
+    }
+    @Test
+    void testHorasEntre12e17(){
+        assertEquals("Afternoon",EstruturaIfElseRefatorado.getMensagemPorHora(12));
+        assertEquals("Afternoon",EstruturaIfElseRefatorado.getMensagemPorHora(17));
+    }
+
+    @Test
+    void testHorasAcimaDe17(){
+        assertEquals("Good evening", EstruturaIfElseRefatorado.getMensagemPorHora(18));
+        assertEquals("Good evening", EstruturaIfElseRefatorado.getMensagemPorHora(23));
+    }
+
+}
